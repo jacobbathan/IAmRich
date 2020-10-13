ec91187ef04360b4d155ac53129f4d5c4f56c381
Jacob Bathan <jacobbathan@Jacobs-MacBook-Pro.local>
2020-10-12 22:54:41 -0700

    init commit and probably the last for this app lol

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Jacob Bathan on 10/4/20.
+//  Copyright © 2020 Jacob Bathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
